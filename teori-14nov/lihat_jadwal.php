e64b36c4621ba4a8e621fb0ffb2ebc1f0abdc2ee
synraax <caesariodito2014@gmail.com>
2022-11-15 00:00:24 +0700

    finish teori 14 nov

--- teori-14nov/lihat_jadwal.php
+++ teori-14nov/lihat_jadwal.php
@@ -2,15 +2,15 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>205314159 - Lihat Jadwal</title>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>205314159 - Lihat Jadwal</title>
 </head>
 
 <body>
 
-    <?php
+	<?php
 
     $servername = "localhost";
     $username = "root";
@@ -25,62 +25,75 @@
     // echo "Connection success <br>";
     ?>
 
-    <h1>Lihat Jadwal</h1>
+	<h1>Lihat Jadwal</h1>
 
-    <?php
-    if(!isset($_GET['filterHari'])){
+	<?php
+    if (!isset($_GET['filterHari'])) {
         $_GET['filterHari'] = "Semua";
-    } else {
-        $filterHari = $_GET['filterHari'];
     }
+    $filterHari = $_GET['filterHari'];
     ?>
 
-    <form action="lihat_jadwal.php" method="get">
-        <label for="jadwalHari">Lihat Jadwal Hari: </label>
-        <select name="filterHari">
-            <option value="none" selected disabled hidden>Pilih Hari</option>
-            <option value="Semua">Semua</option>
-            <option value="Senin">Senin</option>
-            <option value="Selasa">Selasa</option>
-            <option value="Rabu">Rabu</option>
-            <option value="Kamis">Kamis</option>
-            <option value="Jumat">Jumat</option>
-        </select>
-        <input type="submit" value=">">
-    </form>
-        
-    <div class="jadwal">
+	<form action="lihat_jadwal.php" method="get">
+		<label for="jadwalHari">Lihat Jadwal Hari: </label>
+		<select name="filterHari">
+			<option value="none" selected disabled hidden>Pilih Hari</option>
+			<option value="Semua">Semua</option>
+			<option value="Senin">Senin</option>
+			<option value="Selasa">Selasa</option>
+			<option value="Rabu">Rabu</option>
+			<option value="Kamis">Kamis</option>
+			<option value="Jumat">Jumat</option>
+		</select>
+		<input type="submit" value=">">
+	</form>
 
-    <?php
+	<div class="jadwal">
 
-    echo "<h2>$filterHari</h2>";
+		<?php
+        if ($filterHari != 'Semua') {
+            $sql = "SELECT kode, matakuliah, kelas, pengampu, hari, jam FROM jadwal WHERE hari='$filterHari'";
 
-    if($filterHari != 'Semua'){
-        $sql = "SELECT kode, matakuliah, kelas, pengampu, hari, jam FROM jadwal WHERE hari='$filterHari'";
-        echo $sql;
-    } else {
-        $sql = "SELECT kode, matakuliah, kelas, pengampu, hari, jam FROM jadwal";
-        echo $sql;
-    }
+            $result = mysqli_query($conn, $sql);
 
-    $result = mysqli_query($conn, $sql);
+            echo "<ol>";
+            if (mysqli_num_rows($result) > 0) {
+                while ($row = mysqli_fetch_assoc($result)) {
+                    echo  "<li>" . $row["kode"] . " : " . $row["matakuliah"] . " : " . $row["kelas"] . " : " . $row["pengampu"] . " : " . $row["jam"];
+                }
+            } else {
+                echo "<p>Tidak ada</p>";
+            }
+            echo "</ol>";
+        } else {
 
-    echo "<ol>";
-    if (mysqli_num_rows($result) > 0) {
-        while ($row = mysqli_fetch_assoc($result)) {
-            echo  "<li>" . $row["kode"] . " : " . $row["matakuliah"] . " : " . $row["kelas"] . " : " . $row["pengampu"] . " : " . $row["jam"];
+            $arrayHari = ['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu'];
+
+            foreach ($arrayHari as $hari) {
+                $sql = "SELECT kode, matakuliah, kelas, pengampu, hari, jam FROM jadwal WHERE hari='$hari'";
+
+                $result = mysqli_query($conn, $sql);
+
+                echo "<h2>$hari</h2>";
+
+                echo "<ol>";
+                if (mysqli_num_rows($result) > 0) {
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        echo  "<li>" . $row["kode"] . " : " . $row["matakuliah"] . " : " . $row["kelas"] . " : " . $row["pengampu"] . " : " . $row["jam"];
+                    }
+                } else {
+                    echo "<p>Tidak ada</p>";
+                }
+                echo "</ol>";
+            }
         }
-    } else {
-        echo "<td>0 results</td>";
-    }
-    echo "</ol>";
-    // $conn=>close();
-    mysqli_close($conn);
-    ?>
-    </div>
 
-    <br>
-    <a href="input_jadwal.php">| Input Jadwal Kuliah |</a>
+        mysqli_close($conn);
+        ?>
+	</div>
+
+	<br>
+	<a href="input_jadwal.php">| Input Jadwal Kuliah |</a>
 
 </body>
 
